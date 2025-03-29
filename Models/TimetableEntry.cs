5c80f7bfd61627e613045cd90b71f3bad54fa6fe
Just-ADev-Real <74551497+Just-ADev-Real@users.noreply.github.com>
2025-03-29 17:51:40 +0000

    fix: refactored grid implementation, as rows weren't consistent due to task name differences of length

---
+++ Models/TimetableEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace IntuitiveTimetable.Models
+{
+    public class TimetableEntry
+    {
+
+        public string StartTime { get; set; }  
+
+        public string EndTime { get; set; }
+
+        public string TaskName { get; set; }
+    }
+}
--- Pages/Timetable.razor.cs
+++ Pages/Timetable.razor.cs
@@ -1,6 +1,25 @@
-﻿namespace IntuitiveTimetable.Pages
+﻿using IntuitiveTimetable.Models;
+using System.ComponentModel;
+
+namespace IntuitiveTimetable.Pages
 {
     public partial class Timetable
     {
+
+        public List<TimetableEntry> timetableEntries = new List<TimetableEntry>
+        {
+            new TimetableEntry
+            {
+                StartTime = "6:00 AM",
+                EndTime = "6:30 AM",
+                TaskName = "Breakfast"
+            },
+            new TimetableEntry
+            {
+                StartTime = "6:30 AM",
+                EndTime = "7:00 AM",
+                TaskName = "Gym"
+            }
+        };
     }
 }
