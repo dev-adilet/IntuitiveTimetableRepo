4e2e590286645b67b44722517cff707fc88a8f9f
Just-ADev-Real <74551497+Just-ADev-Real@users.noreply.github.com>
2025-04-20 13:28:18 +0100

    added files for deployment

---
+++ js/download.js
@@ -0,0 +1,12 @@
+﻿// wwwroot/js/download.js
+window.downloadFile = (filename, content) => {
+    const blob = new Blob([content], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = filename;
+    anchor.click();
+
+    URL.revokeObjectURL(url);
+};
